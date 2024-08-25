07c143293adf0ea703c7142d17865c035599d1f1
Gabriel Obasi <gabrielobasi557@gmail.com>
2024-08-25 16:54:14 +0100

    Nwq commit

---
+++ app/components/inputList.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+
+type Wish = {
+    wish: string,
+    amount: number
+}
+
+const InputList = () => {
+    const [ wish, setWish ] = useState("");
+    const [ wishList, setWishlist ] = useState<Wish[]>([]);
+    const [ amount, setAmount ] = useState(0);
+
+
+    const handleAddWish = () => {
+        if (wish && amount) {
+            const updatedWishlist = [...wishList, { wish, amount}]
+            setWishlist(updatedWishlist);
+            setWish('');
+            setAmount(0);
+        }
+    };
+    
+    const caluculateTotalAmount = () => {
+        return wishList.reduce((total: any, item: { amount: any; }) => total + item.amount, 0).toFixed(2);
+    };
+
+    const generateShareableLink = () => {
+        const encodedWish = encodeURIComponent(JSON.stringify(wishList));
+        const shareableLink = `${window.location.origin}/?data=${encodedWish}`
+        console.log(shareableLink)
+        alert(`Shareable Link: ${shareableLink}`);
+    };
+    
+  return (
+    <div>
+        <h1>Wishlist</h1>
+        <input 
+          type='text'
+          value={wish}
+          onChange={(e) => setWish(e.target.value)}
+          placeholder='Add your wish'
+        />
+        <input 
+          type='number'
+          value={amount}
+          onChange={(e) => setAmount(e.target.valueAsNumber)}
+          placeholder='Add the price of the wish'
+        />
+        <button onClick={handleAddWish}>Add Wish</button>
+      <div>
+        <ul>
+          {wishList.map((item: any, index: number) => (
+            <li key={index}>
+              {item.wish}: {item.amount} Sol
+            </li>
+          ))}
+        </ul>
+      </div>
+      <h1>Total: {caluculateTotalAmount()} Sol</h1>
+      {wishList.length > 0 && (
+        <div>
+          <button onClick={generateShareableLink}>Generate Link</button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default InputList
---
+++ app/components/outputList.tsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import wishList from "./inputList"
+
+// function outputList({ wishList: [] }) {
+//   return (
+//     <div>
+//         {wishList.map((item: any, index: number) => (
+//             <li key={index}>
+//               {item.wish}: {item.amount} Sol
+//             </li>
+//           ))}
+//     </div>
+//   )
+// }
+
+// export default outputList
